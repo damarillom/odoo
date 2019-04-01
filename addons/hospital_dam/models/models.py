d2edde9ae69af4e7a50cd5dc80989c186e9aa9b4
Daniel Amarillo <danielamarillo1@hotmail.com>
2019-04-01 18:08:35 +0200

    casi acabadi el 3 de hospital

--- addons/hospital_dam/models/models.py
+++ addons/hospital_dam/models/models.py
@@ -52,7 +52,8 @@ class pacient(models.Model):
 	@api.depends('nom','cognom')
 	def _nom_complet(self):
 		for record in self:
-			record.nom_complet = record.nom + " " + record.cognom
+			if (record.nom and record.cognom):
+				record.nom_complet = record.nom + " " + record.cognom
 	
 	@api.depends('doctor_rel')
 	def _doctors(self):
@@ -62,12 +63,30 @@ class pacient(models.Model):
 				num += 1
 			record.num_doctor = num
 			num = 0 
-			
-	@api.constrains('dni')
+	
+	"""@api.constrains('dni')
 	def _check_dni(self):
 		for r in self:
-			if len(self.dni)!= 9:
-				raise ValidationError("DNI: Ha de tenir 9 caràcters")
+			if len(self.dni)!=9:
+				raise ValidationError("DNI: Ha de tenir 9 caràcters")"""
+				
+	@api.onchange('dni')
+	def _correct_dni(self):
+		tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+		numeros = "1234567890"
+		#d = {0:"T" , 1:"R", 2:"W", 3:"A", 4:"G", 5:"M", 6:"I", 7:"F", 8:"P", 9:"D", 10:"X", 11:"B", 12:"N", 13:"J", 14:"Z", 15:"S", 16:"Q", 17:"V", 18:"H", 19:"L", 20:"C", 21:"K", 22:"E"}
+		for r in self:
+			nif = r.dni
+			if (len(nif) == 9):
+				letraControl = nif[8].upper()
+				dni = nif[:8]
+				if ( len(dni) == len( [n for n in dni if n in numeros] ) ):
+					if tabla[int(dni)%23] != letraControl:
+						raise ValidationError("DNI: Letra Incorrecta")
+			"""div = divmod(int(r.dni), 23)
+			letra = d[div[1]] #Letra obtenida
+			if r.dni[:-1] != letra :
+				raise ValidationError("DNI: Letra Incorrecta")""" 
 	
 class Metge(models.Model):
 	_name = 'h2.metge'
